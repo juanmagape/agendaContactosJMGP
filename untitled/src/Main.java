c1a2a26a745fd57054ae24c98de706cc2ff724e1
Juan Manuel Galán Pérez <juanmagape.trb@gmail.com>
2025-09-26 09:35:54 +0200

    Prueba1

---
+++ untitled/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner print = new Scanner(System.in);
+
+        int numberInput = print.nextInt();
+        System.out.println();
+
+
+    }
+}
