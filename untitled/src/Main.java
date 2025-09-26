81d798ec18729bf9b8747becf137172b4aba43b4
Juan Manuel Galán Pérez <juanmagape.trb@gmail.com>
2025-09-26 10:55:56 +0200

    Update Main.java

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -6,8 +6,15 @@ public class Main {
 
         Scanner print = new Scanner(System.in);
 
-        int numberInput = print.nextInt();
-        System.out.println();
+        System.out.println("elige un número");
+        System.out.println("añadir contactos");
+        System.out.println("mostrar contactos");
+        System.out.println("buscar contacto");
+        System.out.println("salir");
+
+        String opcionEscogida = print.nextLine();
+
+        System.out.println("La opción escogida es: " + opcionEscogida);
 
 
     }
