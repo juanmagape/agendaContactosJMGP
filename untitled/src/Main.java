296dad1794f00c139264243d29909617df78bf64
juanmagape <juanmagape.trb.@gmail.com>
2025-10-29 12:49:59 +0100

    Add new features

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -5,17 +5,52 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner print = new Scanner(System.in);
+        boolean salir = true;
+        String nombreContacto = "";
+        int numCon = 0;
 
-        System.out.println("elige un número");
-        System.out.println("añadir contactos");
-        System.out.println("mostrar contactos");
-        System.out.println("buscar contacto");
-        System.out.println("salir");
+        while (salir) {
 
-        String opcionEscogida = print.nextLine();
+        System.out.println("\n===================== AGENDA DE CONTACTOS =====================\n");
+        System.out.println("Escoge un número (1 - 4)\n");
+        System.out.println("1: Añadir contactos");
+        System.out.println("2: Mostrar contactos");
+        System.out.println("3: Buscar contacto");
+        System.out.println("4: Salir");
 
-        System.out.println("La opción escogida es: " + opcionEscogida);
+        int opcionEscogida = print.nextInt();
+        print.nextLine();
 
+            switch (opcionEscogida) {
+                case 1:
+                    System.out.println("Introduce el nombre y el número del contacto");
+                    nombreContacto = print.nextLine();
+                    numCon = print.nextInt();
+                    break;
+                case 2:
+                    System.out.println("Mostrando todos los contactos añadidos...");
+                    System.out.println(nombreContacto + " " + numCon);
+                    break;
+                case 3:
+                    System.out.println("Quieres buscar el contacto por número o por nombre? (1 - 2)");
+                    int busq = print.nextInt();
+
+                    if (busq == 1) {
+                        System.out.println("Introduce el nombre");
+                        String busqNom = print.nextLine();
+                    } else {
+                        System.out.println("Introduce el número");
+                        int busqNum = print.nextInt();
+                    }
+
+                    break;
+                case 4:
+                    System.out.println("Saliendo del programa");
+                    salir = false;
+                    print.close();
+                    break;
+            }
+        }
 
     }
 }
